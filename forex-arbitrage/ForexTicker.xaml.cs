9c440e8c90aa8dab4697762db6c303a27898ac60
chbfiv <chbfiv@gmail.com>
2012-12-30 10:14:48 -0500

    Initial Import

---
+++ forex-arbitrage/ForexTicker.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace forex_arbitrage
+{
+    /// <summary>
+    /// Interaction logic for ForexTicker.xaml
+    /// </summary>
+    public partial class ForexTicker : UserControl
+    {
+        public ForexTicker()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty TickerPriceProperty =
+            DependencyProperty.Register("TickerPrice", typeof(String),
+            typeof(ForexTicker));
+
+        public String TickerPrice
+        {
+            get { return (String)GetValue(TickerPriceProperty); }
+            set { SetValue(TickerPriceProperty, value); }
+        }
+
+        public static readonly DependencyProperty TickerPriceDeltaProperty =
+            DependencyProperty.Register("TickerPriceDelta", typeof(String),
+            typeof(ForexTicker));
+
+        public String TickerPriceDelta
+        {
+            get { return (String)GetValue(TickerPriceDeltaProperty); }
+            set { SetValue(TickerPriceDeltaProperty, value); }
+        }
+    }
+}
---
+++ forex-arbitrage/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace forex_arbitrage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //TODO: hack grid lines colors
+            var T = Type.GetType("System.Windows.Controls.Grid+GridLinesRenderer, PresentationFramework, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
+
+            var GLR = Activator.CreateInstance(T);
+            GLR.GetType().GetField("s_oddDashPen", BindingFlags.Static | BindingFlags.NonPublic).SetValue(GLR, new Pen(Brushes.Gray, 1.0));
+            GLR.GetType().GetField("s_evenDashPen", BindingFlags.Static | BindingFlags.NonPublic).SetValue(GLR, new Pen(Brushes.Gray, 1.0));
+
+            myGrid.ShowGridLines = true;
+        }
+    }
+}
+
